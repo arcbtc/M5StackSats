f38486b53d0250815a3d78141f0d23ad0d8cf742
hika <dev@hiecker.de>
2020-04-13 16:02:10 +0200

    WIP Add calculator feature

--- TheQuickening/M5Stack.h
+++ TheQuickening/M5Stack.h
@@ -1,50 +1,61 @@
-
-
 #include <M5Stack.h> 
 #define KEYBOARD_I2C_ADDR     0X08
 #define KEYBOARD_INT          5
 
 //keypad stuff
-String key_val;
+String key_val,key_val2;
+String calc_sign ="";
+bool calc_flag = false;
 
 void screen_splash()
 {  
-M5.Lcd.drawBitmap(0, 0, 320, 240, (uint8_t *)SplashM5_map);
+  M5.Lcd.drawBitmap(0, 0, 320, 240, (uint8_t *)SplashM5_map);
 }
 
 void screen_input_sats(String fiat, int nosats)
 {
-M5.Lcd.setTextSize(3);
-M5.Lcd.setTextColor(TFT_RED, TFT_BLACK);
-M5.Lcd.setCursor(70, 88);
-M5.Lcd.println(fiat);
-M5.Lcd.setTextColor(TFT_GREEN, TFT_BLACK);
-M5.Lcd.setCursor(87, 136);
-M5.Lcd.println(String(nosats));
+  M5.Lcd.setTextSize(3);
+  M5.Lcd.setTextColor(TFT_RED, TFT_BLACK);
+  M5.Lcd.setCursor(70, 88);
+  M5.Lcd.println(fiat+"        ");
+  M5.Lcd.setTextColor(TFT_GREEN, TFT_BLACK);
+  M5.Lcd.setCursor(87, 136);
+  M5.Lcd.println(String(nosats));
+}
+
+void screen_calc_sats(String fiat, String sign, String calc, String sum)
+{
+  M5.Lcd.setTextSize(3);
+  M5.Lcd.setTextColor(TFT_BLUE, TFT_BLACK);
+  M5.Lcd.setCursor(70, 88);
+  M5.Lcd.println(fiat+" "+sign+" "+calc);
+  M5.Lcd.setTextColor(TFT_BLUE, TFT_BLACK);
+  M5.Lcd.setCursor(87, 136);
+  M5.Lcd.println(sum);
 }
 
 void screen_qrdisplay(String xxx)
 {  
-Serial.println(xxx);
-M5.Lcd.fillScreen(BLACK); 
-M5.Lcd.qrcode(xxx,45,0,240,14);
-delay(100);
+  Serial.println(xxx);
+  M5.Lcd.fillScreen(BLACK); 
+  M5.Lcd.qrcode(xxx,45,0,240,14);
+  delay(100);
 }
 
 void screen_wifi_check()
 {
-M5.Lcd.fillScreen(BLACK);
-M5.Lcd.setCursor(55, 80);
-M5.Lcd.setTextSize(2);
-M5.Lcd.setTextColor(TFT_RED);
-M5.Lcd.println("WIFI NOT CONNECTED");
+  M5.Lcd.fillScreen(BLACK);
+  M5.Lcd.setCursor(55, 80);
+  M5.Lcd.setTextSize(2);
+  M5.Lcd.setTextColor(TFT_RED);
+  M5.Lcd.println("WIFI NOT CONNECTED");
 }
 
 void screen_refresh()
 {
-M5.Lcd.fillScreen(BLACK);
-M5.Lcd.setCursor(0, 0);
-M5.Lcd.setTextColor(TFT_WHITE);
+  M5.Lcd.fillScreen(BLACK);
+  M5.Lcd.setCursor(0, 0);
+  M5.Lcd.setTextColor(TFT_WHITE);
 }
 
 void screen_page_input()
@@ -61,8 +72,26 @@ void screen_page_input()
   M5.Lcd.println("");
   M5.Lcd.println("");
   M5.Lcd.setTextSize(2);
-  M5.Lcd.setCursor(50, 200);
-  M5.Lcd.println("TO RESET PRESS A");
+  M5.Lcd.setCursor(30, 200);
+  M5.Lcd.println("Cancel          OK");
+}
+
+void screen_page_calc()
+{
+  M5.Lcd.fillScreen(BLACK);
+  M5.Lcd.setTextColor(TFT_WHITE);
+  M5.Lcd.setTextSize(3);
+  M5.Lcd.setCursor(0, 40);
+  M5.Lcd.println("Calculator");
+  M5.Lcd.println("");
+  M5.Lcd.println(on_currency.substring(3) + ": ");
+  M5.Lcd.println("");
+  M5.Lcd.println("SUM: ");
+  M5.Lcd.println("");
+  M5.Lcd.println("");
+  M5.Lcd.setTextSize(2);
+  M5.Lcd.setCursor(30, 200);
+  M5.Lcd.println("Cancel    <      OK");
 }
 
 void screen_page_processing()
@@ -76,27 +105,52 @@ void screen_page_processing()
 
 void screen_complete()
 {
-M5.Lcd.fillScreen(BLACK);
-M5.Lcd.setCursor(60, 80);
-M5.Lcd.setTextSize(4);
-M5.Lcd.setTextColor(TFT_GREEN);
-M5.Lcd.println("COMPLETE");
-M5.Speaker.tone(500);
-delay(500);
-M5.Speaker.tone(1000);
-delay(500);
-M5.Speaker.mute();
-delay(2000);
+  M5.Lcd.fillScreen(BLACK);
+  M5.Lcd.setCursor(60, 80);
+  M5.Lcd.setTextSize(4);
+  M5.Lcd.setTextColor(TFT_GREEN);
+  M5.Lcd.println("COMPLETE");
+  M5.Speaker.tone(500);
+  delay(500);
+  M5.Speaker.tone(1000);
+  delay(500);
+  M5.Speaker.mute();
+  delay(2000);
+  M5.Speaker.tone(500);
+  delay(500);
+  M5.Speaker.tone(1000);
+  delay(500);
+  M5.Speaker.mute();
+  delay(2000);
 }
 
 void screen_cancel()
 {
-M5.Lcd.fillScreen(BLACK);
-M5.Lcd.setCursor(50, 80);
-M5.Lcd.setTextSize(4);
-M5.Lcd.setTextColor(TFT_RED);
-M5.Lcd.println("CANCELLED");
-delay(1000);
+  M5.Lcd.fillScreen(BLACK);
+  M5.Lcd.setCursor(50, 80);
+  M5.Lcd.setTextSize(4);
+  M5.Lcd.setTextColor(TFT_RED);
+  M5.Lcd.println("CANCELLED");
+  delay(1000);
+}
+
+void screen_calc()
+{
+  M5.Lcd.fillScreen(BLACK);
+  M5.Lcd.setTextColor(TFT_WHITE);
+  M5.Lcd.setTextSize(3);
+  M5.Lcd.setCursor(0, 40);
+  M5.Lcd.println("Calculator");
+  M5.Lcd.println("");
+  M5.Lcd.println("Test");
+  M5.Lcd.println("");
+  M5.Lcd.println("SATS: ");
+  M5.Lcd.println("");
+  M5.Lcd.println("");
+  M5.Lcd.setTextSize(2);
+  M5.Lcd.setCursor(30, 200);
+  M5.Lcd.println("Cancel          OK");
+  
 }
 
 void get_keypad(){
@@ -110,11 +164,17 @@ void get_keypad(){
        if(key != 0) {
         if(key >= 0x20 && key < 0x7F) { // ASCII String
           if (isdigit((char)key)){
-          key_val = ((char)key);
+            key_val = ((char)key);
           }
+          else if(key == 0x42 || 0x43 || 0x45 || 0x61){ //check if arithmetic sign
+            key_val = ((char)key);
+            calc_sign= key_val;
+            key_val = "";
+            calc_flag = true;
+          } 
           else {
-          key_val = "";
-        } 
+            key_val = "";
+          } 
         }
       }
     }
